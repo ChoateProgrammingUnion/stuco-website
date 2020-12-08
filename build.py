53800110ce7555e3791a97e7b8f658e01e98eff1
Max Fan <theinnovativeinventor@gmail.com>
2020-12-08 16:00:16 -0500

    Added builder function and templates folder

---
+++ build.py
@@ -0,0 +1,5 @@
+import gsimplify
+
+if __name__ == "__main__":
+    builder = gsimplify.build.Builder("0AFB5gF1TxS1vUk9PVA", "templates/")
+    builder.build()
---
+++ templates/script.js
@@ -0,0 +1,11 @@
+/* Credit: https://www.w3schools.com/howto/howto_js_dropdown.asp */
+if($(window).width() < 1000) {
+	function dropdownfunc() {
+		document.getElementById("topnav").classList.toggle("show");
+	}
+	window.onclick = function(event) {
+		if(!event.target.matches('button')) {
+			document.getElementByTagName("nav").remove("show");
+		}
+	}
+}
