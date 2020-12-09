57eea8f82104d60b87d2ac6156dfc2db9f2adabb
imjnnanna <62320912+imjnnanna@users.noreply.github.com>
2020-12-09 11:29:52 -0800

    Actually fixed chrome issue

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,10 @@
 if($(window).width() < 1000) {
 	function dropdownfunc() {
 		document.getElementById("topnav").classList.toggle("show");
-	}
+	}/*
 	window.onclick = function(event) {
 		if(!event.target.matches('button')) {
 			document.getElementById("topnav").remove("show");
 		}
-	}
+	}*/
 }
